d05e4408a42fc0ea636d0bc9868ba22cb6f07d53
radchukv <radchuk.victoria@gmail.com>
2022-08-30 23:57:39 +0200

    inter-coder reliab for the first 3 questions

--- R/2_InterRaterReliab.R
+++ R/2_InterRaterReliab.R
@@ -1,14 +1,45 @@
-## clauclating the inter-rater reliability
-source('./R/0_ReadData.R')
+## calculating the inter-rater reliability, using Cohen's Kappa
+library(readxl)
+library(dplyr)
+library(irr)
+source('./R/Functions.R')
+
+## read in the data coded by the first coder
+files_seb <- list.files(path = ('./data/Seb'), full.names = TRUE)
+
+#bind all data frames of FIRST data frame of read_exl() together
+answers_Seb <- data.frame()
+
+for (file in files_seb) {
+  tmp_exl <- read_exl(file)
+  answers_Seb <- bind_rows(answers_Seb, tmp_exl[[1]])
+}
+row.names(answers_Seb) <- files_seb
+
+
+## read in the data coded by the second coder
+files_mel <- list.files(path = ('./data/Mel'), full.names = TRUE)
+
+#bind all data frames of FIRST data frame of read_exl() together
+answers_Mel <- data.frame()
+
+for (file in files_mel) {
+  tmp_exl <- read_exl(file)
+  answers_Mel <- bind_rows(answers_Mel, tmp_exl[[1]])
+}
+row.names(answers_Mel) <- files_mel
 
 ## for now it seems like 123 study was not entered, so dropping it here (later on check with the coders)
-answers_together <- answers_together[!is.na(answers_together$Q0), ]
-k <- length(unique(answers_together$Q0))
-N <- length(answers_together$Q0)
+answers_Mel <- answers_Mel[!is.na(answers_Mel$Q0), ]
+answers_Seb <- answers_Seb[!is.na(answers_Seb$Q0), ]
 
-## basically we will have to have Q0 two times - one for Seb and one for Mel
-## so, update the function to read the files to differentiate different coders (name of coders as another column);
-## and then rbind them all
-table(answers_together$Q0)
 
-## have to run now
+Q0 <- bind_cols(answers_Seb$Q0, answers_Mel$Q0)
+kappa2(Q0, weight = 'unweighted')
+
+kappa2(bind_cols(answers_Seb$Q1, answers_Mel$Q1))
+kappa2(bind_cols(answers_Seb$Q2, answers_Mel$Q2))
+kappa2(bind_cols(answers_Seb$Q3, answers_Mel$Q3))
+## this metric is only possible to calculate for cases with mutually-exclusive categories
+## OR: one would really go for each subquestion (liek Q3.1, Q3.2 etc)
+kappa2(bind_cols(answers_Seb$Q3.1, answers_Mel$Q3.1))
