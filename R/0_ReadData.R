5ec5ab2c3fe6857c115d368a072a4cbae457da10
radchukv <radchuk.victoria@gmail.com>
2022-08-30 15:13:56 +0200

    clean read data file

---
+++ R/0_ReadData.R
@@ -0,0 +1,22 @@
+## 1. reading in the files
+
+library(readxl)
+library(dplyr)
+source('./R/Functions.R')  ## load functions
+
+files <- list.files(path = ('./data/'), full.names = TRUE)
+
+## test the function
+ch1 <- read_exl('./data/002.xlsx')
+
+
+#bind all data frames of FIRST data frame ofread_exl() together
+answers_together <- data.frame()
+all_data_together <- list()
+
+for (file in files) {
+  tmp_exl <- read_exl(file)
+  answers_together <- bind_rows(answers_together, tmp_exl[[1]])
+}
+row.names(answers_together) <- files
+
