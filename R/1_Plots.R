3cf4986a502249233da9501abaa03f23db503c2a
johannesmarold <maroldjohannes@gmail.com>
2022-08-31 20:17:50 +0200

    finished plots

--- R/1_Plots.R
+++ R/1_Plots.R
@@ -3,6 +3,7 @@
 library(readxl)
 library(dplyr)
 library(ggplot2)
+library(ggalluvial)
 library(gridExtra)
 source('./R/0_ReadData.R')
 
@@ -16,12 +17,25 @@ sum(is.na(answers_together$Q0))
 is.na(answers_together$Q0)
 answers_together$PaperID[is.na(answers_together$Q0)]
 
+## exclude NA sheets
+answers_together <- answers_together[!is.na(answers_together$Q0),]
+
 ## 2. plots
+
+color_chosen <- "green"
+edge_rec <- 0.5
 
 #Q0
 #barplot
 ggplot() +
-  geom_bar(data = answers_together, aes(Q0))
+  geom_bar(data = answers_together, aes(Q0)) +
+  geom_rect(aes(xmin = 1.5, xmax = 2.5,
+                ymin = -edge_rec, ymax = sum(answers_together$Q0 == "Yes")+  edge_rec), 
+            col = color_chosen, alpha = 0) +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank()) 
+
 #simple table with proportions
 Q0_table <- table(answers_together$Q0)
 Q0_table <- cbind(Q0_table,prop.table(Q0_table))
@@ -30,19 +44,52 @@ Q0_table <- cbind(Q0_table,prop.table(Q0_table))
 #Q1
 ggplot() +
   geom_bar(data = answers_together, aes(Q1)) +
-  scale_x_discrete(labels = c("No", "Not as questions, \nbut explicit aim or objective", "Yes", "NA"))
+  geom_rect(aes(xmin = 1.5, xmax = 3.5,
+                ymin = -edge_rec, ymax = max(sum(answers_together$Q0 == "Yes"), sum(answers_together$Q1 == "Not as questions, but explicit aim or obejective"))+edge_rec), 
+            col = color_chosen, alpha = 0) +
+  scale_x_discrete(labels = c("No", "Not as questions, \nbut explicit aim or objective", "Yes", "NA")) +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank()) 
 
 ##RQ1.2
 ##sampling phase
 #Q3
+x_max = length(unique(answers_together$Q3))-0.5
+y_max = max(c(sum(answers_together$Q3 == "Q3.1"), sum(answers_together$Q3 == "Q3.2"),
+              sum(answers_together$Q3 == "Q3.3"), sum(answers_together$Q3 == "Q3.4")))
+
+# add labels of real answer
+for (i in 1:length(answers_together$Q3)) {
+  switch (answers_together$Q3[i],
+    "Q3.1" = answers_together$Q3[i] <- "Journal-Driven",
+    "Q3.2" = answers_together$Q3[i] <- "(public) Database-Driven",
+    "Q3.3" = answers_together$Q3[i] <- "Seminal-Work-Driven",
+    "Q3.4" = answers_together$Q3[i] <- "Others",
+    "Q3.5" = answers_together$Q3[i] <- "Approach unclear",
+  )
+}
+answers_together$Q3 <- factor(answers_together$Q3, levels = c("Combined", "Journal-Driven", "(public) Database-Driven", "Seminal-Work-Driven", "Others", "Approach unclear"))
+
 ggplot() +
-  geom_bar(data = answers_together, aes(Q3))
+  geom_bar(data = answers_together, aes(Q3)) +
+  geom_rect(aes(xmin = 0.5, xmax = x_max,
+                ymin = -edge_rec, ymax = y_max + edge_rec), 
+            col = color_chosen, alpha = 0) +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank())
 
 #Q4
-# comment J: description says only 3 bars? so no NA?
 ggplot() +
   geom_bar(data = answers_together, aes(Q4)) +
-  scale_x_discrete(labels = c("No", "Not keyword \nsearch applied", "Yes", "NA"))
+  scale_x_discrete(labels = c("No", "Not keyword \nsearch applied", "Yes")) +
+  geom_rect(aes(xmin = 1.5, xmax = 3.5,
+                ymin = -edge_rec, ymax = max(sum(answers_together$Q4 == "Yes"),sum(answers_together$Q4 == "No keyword search applied")) + edge_rec), 
+            col = color_chosen, alpha = 0) +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank()) 
 
 #Q10
 answers_together$Q10 <- as.numeric(answers_together$Q10)
@@ -50,18 +97,37 @@ summary(answers_together$Q10)
 
 #Q11
 Q11_sum <- c()
-names_Q11 <- c("Q11.1", "Q11.2", "Q11.3", "Q11.4", "Q11.5")
+#names_Q11 <- c("Q11.1", "Q11.2", "Q11.3", "Q11.4", "Q11.5")
+names_Q11 <- c("not mentioned", "Scopus", "Web of Science", "Google Scholar", "Others")
+
 for (i in 18:21) {
   Q11_sum <- append(Q11_sum, sum((answers_together[i] == "Yes"), na.rm = T))
 }
 Q11_sum <- append(Q11_sum, sum((answers_together$Q11.5 != "No"), na.rm = T))
 names(Q11_sum) <- names_Q11
 
-barplot(Q11_sum)
+y_max = max(Q11_sum)
+
+ggplot(data = data.frame(Q11_sum),aes(seq_along(Q11_sum),Q11_sum)) +
+  geom_bar(stat = "identity") +
+  scale_x_continuous(name = "Q11", breaks = c(1,2,3,4,5), labels = names_Q11) +
+  ylab("count") +
+  geom_rect(aes(xmin = 1.5, xmax = 5.5,
+                ymin = -edge_rec, ymax = y_max + edge_rec), 
+            col = color_chosen, alpha = 0) +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank())
 
 #Q14
 ggplot() +
-  geom_bar(data = answers_together, aes(Q14))
+  geom_bar(data = answers_together, aes(Q14)) +
+  geom_rect(aes(xmin = 1.5, xmax = 2.5,
+                ymin = -edge_rec, ymax =  sum(answers_together$Q14 == "Yes") + edge_rec), 
+            col = color_chosen, alpha = 0) +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank()) 
 
 #Q15-Q20 for supplement
 pQ15 <- ggplot() +
@@ -81,26 +147,57 @@ grid.arrange(pQ15, pQ16, pQ17, pQ18, pQ19, pQ20)
 
 #Q21
 ggplot() +
-  geom_bar(data = answers_together, aes(Q21))
+  geom_bar(data = answers_together, aes(Q21)) +
+  geom_rect(aes(xmin = 1.5, xmax = 2.5,
+                ymin = -0.5, ymax = sum(answers_together$Q21 == "Yes") + 0.5), 
+            col = color_chosen, alpha = 0) +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank()) 
 
 #Q22 for supplement
 ggplot() +
-  geom_bar(data = answers_together, aes(Q22))
+  geom_bar(data = answers_together, aes(Q22)) +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank()) 
 
 #Q27
 Q27_sum <- c()
-names_Q27 <- c("Q27.1", "Q27.2", "Q27.3", "Q27.4", "Q27.5", "Q27.6")
+#names_Q27 <- c("Q27.1", "Q27.2", "Q27.3", "Q27.4", "Q27.6", "Q27.5")
+names_Q27 <- c("Main text\n(method section)", "Footnote", "Appendix", "Supplementary\nmaterial", "Other", "Not at all")
 for (i in 39:44) {
   Q27_sum <- append(Q27_sum, sum((answers_together[i] == "Yes"), na.rm = T))
 }
+# exchange Q27.5 and Q27.6 for rectangel representation
+tmp_Q27.6 <- Q27_sum[6]
+Q27_sum[6] <- Q27_sum[5]
+Q27_sum[5] <- tmp_Q27.6
+
 names(Q27_sum) <- names_Q27
 
+y_max = max(Q27_sum)
 
-barplot(Q27_sum, ylim = c(0,10))
+ggplot(data = data.frame(Q27_sum),aes(seq_along(Q27_sum),Q27_sum)) +
+  geom_bar(stat = "identity") +
+  scale_x_continuous(name = "Q27", breaks = c(1,2,3,4,5,6), labels = names_Q27) +
+  ylab("count") +
+  geom_rect(aes(xmin = 0.5, xmax = 5.5,
+                ymin = -edge_rec, ymax = y_max + edge_rec), 
+            col = color_chosen, alpha = 0) +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank())
 
 #Q30
 ggplot() +
-  geom_bar(data = answers_together, aes(Q30))
+  geom_bar(data = answers_together, aes(Q30)) +
+  geom_rect(aes(xmin = 1.5, xmax = 2.5,
+                ymin = -edge_rec, ymax = sum(answers_together$Q30 == "Yes") + edge_rec), 
+            col = color_chosen, alpha = 0) +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank()) 
 
 #Q32
 answers_together$Q32 <- as.numeric(answers_together$Q32)
@@ -108,7 +205,88 @@ summary(answers_together$Q32)
 
 
 ## Analysis phase
-#Q36 Alluvial plot
+#Q36
+Q36_sum <- c()
+#names_Q11 <- c("Q11.1", "Q11.2", "Q11.3", "Q11.4", "Q11.5")
+names_Q36 <- c("Bibliometric\nanalysis", "Scientific\nmapping", "Meta-analysis", "Structured\ncomparison", "Unstructured\ncomparison", "Others")
+
+for (i in 66:71) {
+  Q36_sum <- append(Q36_sum, sum((answers_together[i] == "Yes"), na.rm = T))
+}
+names(Q36_sum) <- names_Q36
+
+y_max = max(Q36_sum)
+
+ggplot(data = data.frame(Q36_sum),aes(seq_along(Q36_sum),Q36_sum)) +
+  geom_bar(stat = "identity") +
+  scale_x_continuous(name = "Q11", breaks = c(1,2,3,4,5,6), labels = names_Q36) +
+  ylab("count") +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank())
+
+# Alluvial plot for the overlap between the phases
+#Q1
+answers_together$practise_Q1 <- NA
+answers_together$practise_Q1[which(answers_together$Q1 != "No")] <- "Yes"
+answers_together$practise_Q1[which(answers_together$Q1 == "No")] <- "No"
+#Q3
+answers_together$practise_Q3 <- NA
+answers_together$practise_Q3[which(answers_together$Q3 != "Approach unclear")] <- "Yes"
+answers_together$practise_Q3[which(answers_together$Q3 == "Approach unclear")] <- "No"
+#Q4
+answers_together$practise_Q4 <- NA
+answers_together$practise_Q4[which(answers_together$Q4 != "No")] <- "Yes"
+answers_together$practise_Q4[which(answers_together$Q4 == "No")] <- "No"
+#Q11
+answers_together$practise_Q11 <- NA
+answers_together$practise_Q11[which(answers_together$Q11.1 != "Yes")] <- "Yes"
+answers_together$practise_Q11[which(answers_together$Q11.1 == "Yes")] <- "No"
+#Q14
+answers_together$practise_Q14 <- NA
+answers_together$practise_Q14[which(answers_together$Q14 != "No")] <- "Yes"
+answers_together$practise_Q14[which(answers_together$Q14 == "No")] <- "No"
+#Q21
+answers_together$practise_Q21 <- NA
+answers_together$practise_Q21[which(answers_together$Q21 != "No")] <- "Yes"
+answers_together$practise_Q21[which(answers_together$Q21 == "No")] <- "No"
+#Q27
+answers_together$practise_Q27 <- NA
+answers_together$practise_Q27[which(answers_together$Q27.5 != "Yes")] <- "Yes"
+answers_together$practise_Q27[which(answers_together$Q27.5 == "Yes")] <- "No"
+#Q30
+answers_together$practise_Q30 <- NA
+answers_together$practise_Q30[which(answers_together$Q30 != "No")] <- "Yes"
+answers_together$practise_Q30[which(answers_together$Q30 == "No")] <- "No"
+#Q36?
+## set explicit answer for Q36 (assumed, that it's mutual exclusive)
+for (i in 1:length(answers_together$Q36)) {
+  if(is.na(answers_together$Q36[i])){
+    for (j in 66:71) {
+      if(is.na(answers_together[i,j])) {next}
+      if(answers_together[i,j] == "Yes") {
+        answers_together$Q36[i] <- colnames(answers_together)[j]
+      }
+    }
+  }
+}
+
+#actual alluvial plot
+ggplot(data = answers_together, aes(axis1 = practise_Q1, axis2 = practise_Q3, 
+                                    axis3 = practise_Q4, axis4 = practise_Q11, 
+                                    axis5 = practise_Q14, axis6 = practise_Q21, 
+                                    axis7 = practise_Q27, axis8 = practise_Q30,
+                                    axis9 = Q36,
+                                    fill = practise_Q1)) +
+  geom_alluvium(width = 0) +
+  geom_stratum(width = 1/3, fill = "white", color = "grey") +
+  scale_x_discrete(limits = c("Q1", "Q3", "Q4", "Q11", "Q14", "Q21", "Q27", "Q30", "Q36"), expand = c(.05, .05)) +
+  scale_fill_brewer(type = "qual", palette = "Set1") +
+  geom_text(stat = "stratum", aes(label = after_stat(stratum))) +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank()) +
+  ylab("count") +
+  guides(fill=guide_legend(title="Is a research question\nexplicitly formulated?"))
 
 ##RQ3.1
 #Q2
@@ -130,22 +308,37 @@ ggplot() +
 #Q33
 Q33_sum <- c()
 names_Q33 <- c("Q33.1", "Q33.2", "Q33.3", "Q33.4", "Q33.5", "Q33.6", "Q33.7", "Q33.8")
+## Jo: actual names are quite long should I apply them anyways?
+
 for (i in 51:57) {
   Q33_sum <- append(Q33_sum, sum((answers_together[i] == "Yes"), na.rm = T))
 }
 Q33_sum <- append(Q33_sum, sum((answers_together$Q33.8 != "No"), na.rm = T))
 names(Q33_sum) <- names_Q33
 
-barplot(Q33_sum)
+ggplot(data = data.frame(Q33_sum),aes(seq_along(Q33_sum),Q33_sum)) +
+  geom_bar(stat = "identity") +
+  scale_x_continuous(name = "Q27", breaks = c(1,2,3,4,5,6,7,8), labels = names_Q33) +
+  ylab("count") +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank())
 
 #Q34
 Q34_sum <- c()
 names_Q34 <- c("Q34.1", "Q34.2", "Q34.3", "Q34.4")
+## Jo: same as for Q33
+
 for (i in 60:62) {
   Q34_sum <- append(Q34_sum, sum((answers_together[i] == "Yes"), na.rm = T))
 }
 Q34_sum <- append(Q34_sum, sum((answers_together$Q34.4 != "No"), na.rm = T))
 names(Q34_sum) <- names_Q34
 
-barplot(Q34_sum)
-
+ggplot(data = data.frame(Q34_sum),aes(seq_along(Q34_sum),Q34_sum)) +
+  geom_bar(stat = "identity") +
+  scale_x_continuous(name = "Q27", breaks = c(1,2,3,4), labels = names_Q34) +
+  ylab("count") +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank())
