49d1ab9285e8e0a0dae2b3f878a66a6ae4cc5e10
Sebastian Achter <sebastian.achter@tuhh.de>
2023-01-30 14:08:27 +0100

    Test calculation to convert matrices into nxn format is now working. Calculation of AC1 and Kappe now possible even if one coder did not use all answer options. Next, respective adjustment needs to be done in for-loop.

--- R/3_InterRaterReliab.R
+++ R/3_InterRaterReliab.R
@@ -29,6 +29,7 @@ answers_CoderB <- answers_CoderB[!is.na(answers_CoderB$Q0), ]
 
 ### Start: testing calculations for Gwet's AC1, Cohen's kappa and raw (percentage) agreement---------------
 
+## Calculate kappa with irr-package
 Q0 <- bind_cols(answers_CoderA$Q0, answers_CoderB$Q0)
 kappa2(Q0, weight = 'unweighted')
 
@@ -39,32 +40,30 @@ kappa2(bind_cols(answers_CoderA$Q3, answers_CoderB$Q3))
 ## OR: one would really go for each sub-question (like Q3.1, Q3.2 etc)
 kappa2(bind_cols(answers_CoderA$Q3.1, answers_CoderB$Q3.1))
 
-# Calculate raw (percentage) agreement 
+## Calculate raw (percentage) agreement 
 tmp_raw_agree <- sum(ifelse(answers_CoderA$Q1==answers_CoderB$Q1, 1, 0))/nrow(answers_CoderA)
 
-# Calculate Gwet's AC1 and (Kappa) with the irrCAC-package including benchmarking
-tmp_contigency_table <-  as.data.frame.matrix(table(answers_CoderA$Q15, answers_CoderB$Q15)) #create a contingency table
-# Einträge von Tabelle sind vllt Index?? --> Fehler? 
-# Sehe ich auch so!
-print(tmp_contigency_table)
+## Calculate Gwet's AC1 and Kappa with the irrCAC-package including benchmarking
+tmp_contigency_table <-  as.matrix(table(answers_CoderA$Q7, answers_CoderB$Q7)) #create a contingency table
 
+#create support matrix since not always nxn matrix (on coder did not use on of the answer options)
 un1 <- unique(sort(c(colnames(tmp_contigency_table), rownames(tmp_contigency_table))))
 m2 <- matrix(NA, length(un1), length(un1), dimnames = list(un1, un1))
-#m2[row.names(tmp_contigency_table), colnames(tmp_contigency_table)] <- tmp_contigency_table
 cols <- colnames(m2)[colnames(m2) %in% colnames(tmp_contigency_table)]
 rows <- rownames(m2)[rownames(m2) %in% rownames(tmp_contigency_table)]
 m2[rows, cols] <- tmp_contigency_table[rows, cols]
-m2
-print(m2)
+m2[is.na(m2)] <- 0
+
+tmp_contigency_table <- m2
 
 tmp_gwet <- gwet.ac1.table(tmp_contigency_table)
-tmp_gwet_bm <- landis.koch.bf(tmp_gwet$coeff.val,tmp_gwet$coeff.se) 
+#tmp_gwet_bm <- landis.koch.bf(tmp_gwet$coeff.val,tmp_gwet$coeff.se) 
 tmp_kappa <- kappa2.table(tmp_contigency_table)
-tmp_kappa_bm <- landis.koch.bf(tmp_kappa$coeff.val,tmp_kappa$coeff.se) 
-print(tmp_gwet)
-print(tmp_gwet_bm)
-print(tmp_kappa)
-print(tmp_kappa_bm)
+#tmp_kappa_bm <- landis.koch.bf(tmp_kappa$coeff.val,tmp_kappa$coeff.se) 
+#print(tmp_gwet)
+#print(tmp_gwet_bm)
+#print(tmp_kappa)
+#print(tmp_kappa_bm)
 
 
 
