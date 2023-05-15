72aa8203884536ab16d1f7d8495f0dc7172a9f3e
Sebastian Achter <sebastian.achter@tuhh.de>
2023-05-15 07:26:42 +0200

    new Figure 5.3 about the use of the theory development dimensions

--- R/1_Plots.R
+++ R/1_Plots.R
@@ -7,6 +7,9 @@ library(ggalluvial)
 library(gridExtra)
 library(scales)
 library(patchwork)
+library(reshape2)
+library(cowplot)
+library(svglite)
 source('./R/0_ReadData.R')
 
 
@@ -388,95 +391,6 @@ answers_together$practise_Q36 <- NA
 answers_together$practise_Q36[which(answers_together$Q36.5 != "Yes")] <- "Yes"
 answers_together$practise_Q36[which(answers_together$Q36.5 == "Yes")] <- "No"
 
-#Q36?
-## set explicit answer for Q36 (assumed, that it's mutual exclusive)
-#for (i in 1:length(answers_together$Q36)) {
-#  if(is.na(answers_together$Q36[i])){
-#    for (j in 66:71) {
-#      if(is.na(answers_together[i,j])) {next}
-#      if(answers_together[i,j] == "Yes") {
-#        answers_together$Q36[i] <- colnames(answers_together)[j]
-#      }
-#    }
-#  }
-#}
-
-#actual alluvial plot: full version
-ggplot(data = answers_together, aes(axis1 = practise_Q1, axis2 = practise_Q3, 
-                                    axis3 = practise_Q4, axis4 = practise_Q11, 
-                                    axis5 = practise_Q14, axis6 = practise_Q21, 
-                                    axis7 = practise_Q27, axis8 = practise_Q30,
-                                    #axis9 = Q36,
-                                    fill = practise_Q1)) +
-  geom_alluvium(width = 0) +
-  geom_stratum(width = 1/5, fill = "white", color = "grey") +
-  scale_x_discrete(limits = c("Q1", "Q3", "Q4", "Q11", "Q14", "Q21", "Q27", "Q30"), expand = c(.05, .05)) +
-  scale_fill_brewer(type = "qual", palette = "Set1") +
-  geom_text(stat = "stratum", aes(label = after_stat(stratum))) +
-  theme_bw() +
-  theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank()) +
-  ylab("count") +
-  guides(fill=guide_legend(title="Is a research question\nexplicitly formulated?")) +
-  annotate("label", x = 1.08, y = 17, label = "Question\nformulation", size = 2.5)  +
-  annotate("label", x = 4.5, y = 17, label = "Sampling phase", size = 4)  +
-  annotate("label", x = 8, y = 17, label = "Analysis\nphase", size = 2.5)
-
-ggsave("./plots/alluvial_big.pdf")
-
-# shorter version
-ggplot(data = answers_together, aes(axis1 = practise_Q1, axis2 = practise_Q4, 
-                                    axis3 = practise_Q11, axis4 = practise_Q14, 
-                                    axis5 = practise_Q27, 
-                                    #axis8 = Q36,
-                                    fill = practise_Q1)) +
-  geom_alluvium(width = 0) +
-  geom_stratum(width = 1/5, fill = "white", color = "grey") +
-  scale_x_discrete(limits = c("Q1", "Q4", "Q11", "Q14", "Q27"), expand = c(.05, .05)) +
-  scale_fill_brewer(type = "qual", palette = "Set1") +
-  geom_text(stat = "stratum", aes(label = after_stat(stratum))) +
-  theme_bw() +
-  theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank()) +
-  ylab("count") +
-  guides(fill=guide_legend(title="Is a research question\nexplicitly formulated?")) +
-  annotate("label", x = 1.1, y = 17, label = "Question\nformulation", size = 3)  +
-  annotate("label", x = 3, y = 17, label = "Sampling phase", size = 4)  +
-  annotate("label", x = 4.97, y = 17, label = "Analysis\nphase", size = 3)
-
-ggsave("./plots/alluvial_small.pdf")
-
-# Currently used version
-
-# to create the alluvial plots for ecology or social science only filter answers_together
-# for discipline in column "category" (not yet implemented as separate code)
-
-answers_together$practise_phase_2 <- NA
-# add good practise for phase 2: sampling phase
-for (j in 1:length(answers_together$Q0)) {
-  for (i in 83:89) {
-    if(answers_together[j,i] == "No") {
-      answers_together$practise_phase_2[j] = "No"
-      break
-    }
-  }
-}
-answers_together$practise_phase_2[is.na(answers_together$practise_phase_2)] <- "Yes"
-
-ggplot(data = answers_together, aes(axis1 = practise_Q1, axis2 = practise_phase_2, 
-                                    axis3 = practise_Q36,
-                                    fill = practise_Q1)) +
-  geom_alluvium(width = 0) +
-  geom_stratum(width = 1/6, fill = "white", color = "grey") +
-  scale_x_discrete(limits = c("Question\nformulation", "Sampling phase", "Analysis\nphase"), expand = c(.05, .05)) +
-  scale_fill_brewer(type = "qual", palette = "Set1") +
-  geom_text(stat = "stratum", aes(label = after_stat(stratum))) +
-  theme_bw() +
-  theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),
-        legend.position = 'bottom') +
-  ylab("count") +
-  guides(fill=guide_legend(title="Does the paper comply with the 'good practice?'"))
-
-ggsave("./plots/alluvial_finSeb.pdf")
-
 # updated version for separated alluvial plots
 
 # needs to reorder the data
@@ -502,7 +416,7 @@ p1 <- ggplot(alluvial_data[alluvial_data$category == "ecology",],
   geom_flow(stat = "alluvium", lode.guidance = "frontback",
             color = "darkgray") +
   geom_stratum(fill = "white", color = "grey") +
-  scale_x_discrete(labels = c("Question\nformulation", "Sampling\nphase", "Analysis\nphase", "Overall"), expand = c(.05, .05)) +
+  scale_x_discrete(labels = c("Question\nformulation", "Sampling\nstep", "Analysis\nstep", "Overall"), expand = c(.05, .05)) +
   geom_text(stat = "stratum", size = 3) +
   guides(fill="none") +
   theme_bw() +
@@ -510,10 +424,10 @@ p1 <- ggplot(alluvial_data[alluvial_data$category == "ecology",],
         axis.title = element_text(size = 8), plot.title = element_text(size = 8),
         legend.title = element_text(size=8),axis.text.x = element_text(size =8, color = "black"),
         axis.text.y = element_text(size =8, color = "black")) +
-  ylab("Article Sample") +
+  ylab("Number of articles") +
   xlab("SLR steps") +
   #guides(fill=guide_legend(title="Does the paper comply with the 'good practice?'")) +
-  ggtitle("a)")
+  ggtitle("Ecology")
 
 p2 <- ggplot(alluvial_data[alluvial_data$category == "social",], 
        aes(x = phase, stratum = answers, alluvium = studies,
@@ -522,7 +436,7 @@ p2 <- ggplot(alluvial_data[alluvial_data$category == "social",],
   geom_flow(stat = "alluvium", lode.guidance = "frontback",
             color = "darkgray") +
   geom_stratum(fill = "white", color = "grey") +
-  scale_x_discrete(labels = c("Question\nformulation", "Sampling\nphase", "Analysis\nphase", "Overall"), expand = c(.05, .05)) +
+  scale_x_discrete(labels = c("Question\nformulation", "Sampling\nstep", "Analysis\nstep", "Overall"), expand = c(.05, .05)) +
   geom_text(stat = "stratum", size = 3) +
   theme(legend.position = "bottom") +
   theme_bw() +
@@ -530,10 +444,10 @@ p2 <- ggplot(alluvial_data[alluvial_data$category == "social",],
         axis.title = element_text(size = 8), legend.position = 'bottom',plot.title = element_text(size = 8),
         axis.text.x = element_text(size =8, color = "black"), axis.text.y = element_text(size =8, color = "black"),
         legend.title = element_text(size=8)) +
-  ylab("Article Sample") +
+  ylab("Number of articles") +
   xlab("SLR steps") +
   guides(fill=guide_legend(title="Does the paper comply with 'good practice?' ")) +
-  ggtitle("b)")
+  ggtitle("Social Sciences")
 
 p1 / p2
 
@@ -567,7 +481,72 @@ ggplot(data = answers_together, aes(x = Q33.7)) +
 
 ggsave("./plots/Q33.7.pdf")
 
-##RQ3.1
+#Q33 new Figure 5.3
+
+#theory <- read_excel("fig53.xlsx")
+Q33_df <- select(answers_together, PaperID, category, Q33.1, Q33.2, Q33.3, Q33.4, Q33.5, Q33.6, Q33.7)
+Q33_df <- Q33_df %>% rename("Dim-1" = "Q33.1",
+                            "Dim-2" = "Q33.2",
+                            "Dim-3" = "Q33.3",
+                            "Dim-4" = "Q33.4",
+                            "Dim-5" = "Q33.5",
+                            "Dim-6" = "Q33.6",
+                            "Dim-7" = "Q33.7")
+
+Q33_df_long <- gather(Q33_df, Dimension, Discussed, `Dim-1`:`Dim-7`, factor_key=TRUE)
+
+ecology <- Q33_df_long %>% filter(category == "ecology")
+social <- Q33_df_long %>% filter(category == "social")
+
+pQ33_ecol <- ggplot(data = ecology, aes(x=PaperID, y=Dimension, fill=Discussed)) + 
+  geom_tile(colour="white") +
+  coord_fixed(ratio=1) +
+  scale_fill_manual(values=c("grey90", "#3EB489")) + 
+  labs(x = "Article ID", y = "Theory\nDimensions") + 
+  theme(legend.position = "none", axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=0.5)) 
+pQ33_social <- ggplot(data = social, aes(x=PaperID, y=Dimension, fill=Discussed)) + 
+  geom_tile(colour="white") + 
+  coord_fixed(ratio=1) +
+  scale_fill_manual(values=c("grey90", "#3EB489")) + 
+  labs(x = "Article ID", y = "Theory\nDimensions") + 
+  theme(legend.position = "bottom", axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=0.5))
+
+# Arrange the plots in one graph with cowplots
+# Here we use library("cowplot")
+ggdraw() +
+  draw_plot(pQ33_ecol, x = 0, y = .55, width = 1, height = .3) +
+  draw_plot(pQ33_social, x = 0, y = .1, width = 1, height = .4) +
+  draw_plot_label(label = c("Ecology", "Social Sciences"), size = 10,
+                  x = c(.28, .39), y = c(.87, 0.52))
+
+ggsave("./plots/fig53.svg", width = 1000, height=600, limitsize = FALSE)
+
+##################################
+
+#Q34 (bar chart)
+Q34_sum <- c()
+names_Q34 <- c("Design perspective", "Insight perspective", "Effect perspective", "Other")
+
+for (i in 60:62) {
+  Q34_sum <- append(Q34_sum, sum((answers_together[i] == "Yes"), na.rm = T))
+}
+Q34_sum <- append(Q34_sum, sum((answers_together$Q34.4 != "No"), na.rm = T))
+names(Q34_sum) <- names_Q34
+
+ggplot(data = data.frame(Q34_sum),aes(seq_along(Q34_sum),Q34_sum)) +
+  geom_bar(stat = "identity") +
+  geom_text(aes(label = Q34_sum), vjust=-0.5) +
+  scale_x_continuous(name = "Q34", breaks = c(1,2,3,4), labels = names_Q34) +
+  scale_y_continuous(breaks= pretty_breaks()) +
+  ylab("count") +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.text.x = element_text(angle = 45, hjust=1))
+
+ggsave("./plots/Q34.pdf")
+
+
 #Q33
 Q33_sum <- c()
 names_Q33 <- c("Description of or new\ninsights about mechanisms", "Behavioral model comparisons",
@@ -594,7 +573,9 @@ ggplot(data = data.frame(Q33_sum),aes(seq_along(Q33_sum),Q33_sum)) +
 
 ggsave("./plots/Q33.pdf")
 
-#Q33 from paper fig.5.3
+############################################################################################
+
+#Q33 old version of Figure 5.3
 Q33_df <- data.frame(
   c(rep("Q33.1", 42), rep("Q33.2", 42), rep("Q33.3", 42), rep("Q33.4", 42),
     rep("Q33.5", 42), rep("Q33.6", 42), rep("Q33.7", 42), rep("Q33.8", 42)),
@@ -640,27 +621,4 @@ p2 <- ggplot() +
 p1 + p2
 
 ggsave("./plots/Q33_fig5.3.pdf")
-
-#Q34
-Q34_sum <- c()
-names_Q34 <- c("Design perspective", "Insight perspective", "Effect perspective", "Other")
-
-for (i in 60:62) {
-  Q34_sum <- append(Q34_sum, sum((answers_together[i] == "Yes"), na.rm = T))
-}
-Q34_sum <- append(Q34_sum, sum((answers_together$Q34.4 != "No"), na.rm = T))
-names(Q34_sum) <- names_Q34
-
-ggplot(data = data.frame(Q34_sum),aes(seq_along(Q34_sum),Q34_sum)) +
-  geom_bar(stat = "identity") +
-  geom_text(aes(label = Q34_sum), vjust=-0.5) +
-  scale_x_continuous(name = "Q34", breaks = c(1,2,3,4), labels = names_Q34) +
-  scale_y_continuous(breaks= pretty_breaks()) +
-  ylab("count") +
-  theme_bw() +
-  theme(panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        axis.text.x = element_text(angle = 45, hjust=1))
-
-ggsave("./plots/Q34.pdf")
 
